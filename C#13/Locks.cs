d1c591e0616f45c11793a1325e8aaf695d3d2629
devanshi <devanshi.bhatevara@simformsolutions.com>
2025-04-23 16:51:27 +0530

    added extra comments

--- C#13/Locks.cs
+++ C#13/Locks.cs
@@ -2,9 +2,10 @@
 {
     public class Locks
     {
+        //using general object
         private static readonly object _oldLock = new();
 
-        //We used to use the general object type for lock but now we have a dedicated Lock type
+        //We used to use the general object type for lock but now we have a dedicated Lock type - System.Threading.Lock
         private static readonly Lock _newLock = new Lock();
 
         public static void Run()
@@ -18,6 +19,7 @@
 
             Console.WriteLine("New lock object using System.Threading");
 
+            //Only one thread can execute this block at a time
             lock (_newLock)
             {
                 Console.WriteLine("We're inside new lock");
